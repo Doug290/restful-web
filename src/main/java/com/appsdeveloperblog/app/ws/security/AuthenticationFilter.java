882b2cb41e518c06180a77081fb0f2a43f69ec96
Douglas <frc200_2@hotmail.com>
2019-02-09 17:38:06 -0200

    bug fixed wuth filter

--- src/main/java/com/appsdeveloperblog/app/ws/security/AuthenticationFilter.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/AuthenticationFilter.java
@@ -9,6 +9,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.appsdeveloperblog.ws.ui.model.request.UserLoginRequestModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,21 +21,16 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.appsdeveloperblog.ws.ui.model.request.UserLoginRequestModel;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 	private final AuthenticationManager authenticationManager;
 	
 	public AuthenticationFilter(AuthenticationManager authenticationManager) {
 		this.authenticationManager = authenticationManager;
 	}
-	
-	public Authentication attempAuthentication(HttpServletRequest req,
-												HttpServletResponse res) throws AuthenticationException {
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest req,
+											   HttpServletResponse res) throws AuthenticationException {
 		try {
 			UserLoginRequestModel creds = new ObjectMapper()
 					.readValue(req.getInputStream(), UserLoginRequestModel.class);
@@ -51,9 +51,8 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 											HttpServletResponse res,
 											FilterChain chain,
 											Authentication auth) throws IOException, ServletException {
+
 		String userName = ((User) auth.getPrincipal()).getUsername();
-		//String tokenSecret = new SecurityConstants().getTokenSecret();
-		
 		String token = Jwts.builder()
 				.setSubject(userName)
 				.setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
